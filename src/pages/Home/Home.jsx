fee4b8300d3354890ad4cc5e7a2aa0517c8e1707
Felipe Mendes <felipemendes0411@gmail.com>
2022-11-02 21:36:33 -0300

    add logout in home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useContext} from "react";
 
+import { AuthContext } from "../../context/auth";
 const Home = () => {
+    const { authenticated,logout } = useContext(AuthContext)
+    const handleLogout = () =>{
+        logout();
+    }
     return (
-        <h1>Page Home</h1>
+        <>
+            <h1>Page Home</h1>
+            <p>{String(authenticated)}</p>
+            <button onClick={handleLogout}>Logout</button>
+        </>
     )
 }
 
